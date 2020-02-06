a25ce421dc1b353e46290e23371f85592a02d295
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2020-02-06 16:33:16 +0200

    add swager doc and the js docs and the UML

--- src/middleware/404.js
+++ src/middleware/404.js
@@ -1,4 +1,15 @@
+/**
+ * 404 Middleware
+ * @module middleware/404
+ */
 
+/**
+ * Sends a JSON Formatted 404 Response
+ * @param req {object} Express Request Object
+ * @param res {object} Express Response Object
+ * @param next {function} Express middleware next()
+ */
+// eslint-disable-next-line no-unused-vars
 module.exports = (req,res,next) => {
   let error = { error: 'Resource Not Found' };
   res.status(404).json(error).end();
